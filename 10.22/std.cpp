79946610e4271b33fc87336c30f20cadfdea3ef3
3m1p <micoael@qq.com>
2020-10-26 12:59:05 +0800

    ok

--- 10.22/std.cpp
+++ 10.22/std.cpp
@@ -15,23 +15,17 @@ struct Edge {int to, nxt;} e[N];
 void adde(int u, int v) {e[++cnt].to = v;e[cnt].nxt = head[u];head[u] = cnt;}
 void dfs(int u, int fa) {
   int an2 = ans2, pvv = -1, bkk = bk, com = 0, an1 = ans1, fstt = fst;s[++tp] = alp[u];
-  if (u == 1 && s[tp] == 0) {
-    ans2 = mul(ans2, k);an2 = ans2;combo = 1;finalans ^= mul(ans2, ans1);
-  } else if (s[tp] == 0) {
-    if (!combo) {pv = tp - 1;pvv = pv;} else {pvv = pv;}
+  if (u == 1 && s[tp] == 0) {ans2 = mul(ans2, k);an2 = ans2;combo = 1;finalans ^= mul(ans2, ans1);
+  } else if (s[tp] == 0) {if (!combo) {pv = tp - 1;pvv = pv;} else {pvv = pv;}
     combo = 1;com = 1;ans2 = mul(ans2, (k - 1));an2 = ans2;finalans ^= mul(ans2, ans1);
-  } else {
-    bk = tp;bkk = bk;
+  } else {bk = tp;bkk = bk;
     if (combo) {
       if (pv == -1 || fst) {ans1 = mul(1, qpow(k - 1, bk - 1));an1 = ans1;
       } else if (s[pv] == s[bk]) {ans1 = mul(ans1, f[1][bk - pv - 1]);an1 = ans1;
       } else {ans1 = mul(ans1, f[0][bk - pv - 1]);an1 = ans1;
-	  }
-      finalans ^= ans1;
+	  }finalans ^= ans1;
     } else {
-      if (fst) {ans1 = qpow(k - 1, tp - 1);an1 = ans1;finalans ^= ans1;
-      } else { finalans ^= ans1;
-      }
+      if (fst) {ans1 = qpow(k - 1, tp - 1);an1 = ans1;finalans ^= ans1;} else { finalans ^= ans1;}
     }
     pv = tp, bk = -1;pvv = pv, bkk = bk;combo = false;com = combo;fstt = fst = 0;ans2 = 1;an2 = ans2;
   }
@@ -45,15 +39,12 @@ void dfs(int u, int fa) {
 
 signed main() {
 #ifndef ONLINE_JUDGE
-  freopen("D:/Testcases/in.ac", "r", stdin);
-  freopen("D:/Testcases/out.ac", "w", stdout);
-#else
+  freopen("D:/Testcases/in.ac", "r", stdin);freopen("D:/Testcases/out.ac", "w", stdout);
 #endif
   cin >> n >> k;
   for (int i = 1; i <= n; i++) cin >> alp[i];
   for (int i = 1; i < n; i++) {int x;cin >> x;adde(i + 1, x);adde(x, i + 1);}
   f[0][0] = 1;
   for (int i = 1; i <= n; i++) {f[0][i] = (int)add(f[1][i - 1], mul((k - 2), f[0][i - 1]));f[1][i] = (int)mul((k - 1), f[0][i - 1]);}
-  dfs(1, 0);
-  printf("%lld\n", finalans);
+  dfs(1, 0);printf("%lld\n", finalans);
 }
