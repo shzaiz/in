5d998afc079923839086a6591d48e90507ae1069
3m1p <micoael@qq.com>
2020-10-27 10:05:32 +0800

    这场比赛应该可以AK😀

---
+++ 10.27/a.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define D double
+#define N 500010
+int fa[N];
+int S,P;
+int cnt = 0;
+int find(int x){
+    return x==fa[x] ? x: fa[x] = find(fa[x]);
+}
+bool unite(int x,int y){
+    int xx = find(x);
+    int yy = find(y);
+    if(xx==yy) return 0;
+    else fa[yy] = xx;
+}
+bool same(int x,int y){
+    return find(x) == find(y);
+}
+int x[N],y[N];
+struct Edge{
+    int frm,to,nxt;
+    D w;
+}e[N];
+D dist(D x1,D x2,D y1,D y2){
+    return sqrt(abs((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)));
+}
+bool cmp(Edge x,Edge y){
+    return x.w<y.w;
+}
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("D:/Testcases/in.ac","r",stdin);
+    freopen("D:/Testcases/out.ac","w",stdout);
+    #else
+    freopen("wireless.in","r",stdin);
+    freopen("wireless.out","w",stdout);
+    #endif
+    for(int i = 0;i<=N;i++) fa[i] = i;
+    cin>>S>>P;
+    for(int i = 1;i<=P;i++){
+        cin>>x[i]>>y[i];
+        for(int j = 1;j<i;j++){
+            e[++cnt].frm = j;
+            e[cnt].to = i;
+            e[cnt].w = dist(x[i],x[j],y[i],y[j]);
+            // printf("%d,%d = %lf\n",i,j,dist(x[i],x[j],y[i],y[j]));
+        }
+    }
+    sort(e+1,e+cnt+1,cmp);
+    // for(int i = 1;i<=cnt;i++) printf("[%d --> %d] = %lf\n",e[i].frm,e[i].to,e[i].w);
+    int chose = 0;
+    D ans = 0;
+    for(int i =1;i<=cnt;i++){
+        int u = e[i].to; int v = e[i].frm;
+        if(!same(u,v)){
+            unite(u,v);
+            ans = e[i].w;
+            chose++;
+            if(chose == S) break;
+        }
+    }
+    cout<<fixed<<setprecision(2)<<ans<<endl;
+}
---
+++ 10.27/b.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define tm fuckccf
+#define N 100010
+struct Edge {
+    int to, nxt, w;
+} e[N];
+int head[N];
+int cnt = 0;
+int dis[N], vis[N], tm[N];
+void adde(int u, int v, int w)
+{
+    // printf("Added %d --[%d]--> %d\n ", u, w, v);
+    e[++cnt].to = v;
+    e[cnt].nxt = head[u];
+    e[cnt].w = w;
+    head[u] = cnt;
+}
+int n, m;
+queue<int> Q;
+
+bool spfa(int s)
+{
+    memset(dis, 1e9 + 7, sizeof dis);
+    dis[s] = 0;
+    vis[s] = 1;
+    Q.push(s);
+    while (!Q.empty()) {
+        int u = Q.front();
+        vis[u] = 0;
+        Q.pop();
+        for (int i = head[u]; i; i = e[i].nxt) {
+            int v = e[i].to;
+            if (!vis[v] && dis[u] + e[i].w < dis[v]) {
+                dis[v] = dis[u] + e[i].w;
+                Q.push(v);
+                tm[v]++;
+                vis[v] =1;
+                if (tm[v] >= n)
+                    return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+freopen("D:/Testcases/in.ac","r",stdin);
+freopen("D:/Testcases/out.ac","w",stdout);
+#else
+freopen("farm.in","r",stdin);
+freopen("farm.out","w",stdout);
+#endif
+    cin >> n >> m;
+    int op;
+    
+    for (int i = 1; i <= m; i++) {
+        cin >> op;
+        if (op == 3) {
+            int a, b;
+            cin >> a >> b;
+            adde(a, b, 0);
+            adde(b, a, 0);
+        } else if (op == 1) {
+            int a, b, c;
+            cin >> a >> b >> c;
+            adde(a, b, -c);
+        } else if (op == 2) {
+            int a, b, c;
+            cin >> a >> b >> c;
+            adde(a, b, c);
+        } else {
+            printf("Your fucking idiot data must made wrong!!!");
+        }
+    }
+    if (!spfa(1)) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
+    }
+    return 0;
+}
