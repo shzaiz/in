d0203f1f8d1e2aa8c90a1489b9a3b145c02fef8f
shzaiz <micoael@qq.com>
2020-11-21 13:38:06 +0800

    c++

---
+++ 11.19/problem/T1/hiro.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define 希萝闲逛(i,希萝) for(int i  =  1;i<= 希萝;i++)
+#define 希萝找人(i,b,希萝) for(int i  =  b;i<= 希萝;i++)
+#define re(i,希萝,b) for(int i  =  希萝;i>b;i--)
+#define N 200005
+#define DD 19260817
+#define 剪  %
+#define 耳朵 long long
+#define 然后 +
+#define 反过来 *
+#define 你不对啊 signed
+#define 我打你啊 main
+#define 对希萝说  return
+#define 捉住 cin
+#define 一个 >>
+#define 学习 =  
+#define 放走 cout
+#define 一只 <<
+耳朵 anti[200005],n,k;
+耳朵 单推人[55][200005];
+耳朵 耗子尾汁(耳朵 希萝,耳朵 尾巴){ 对希萝说 (希萝 剪 DD  然后 DD 剪 尾巴) 剪 DD;}
+耳朵 好好反思(耳朵 希萝,耳朵 尾巴){ 对希萝说  (希萝 剪 DD  反过来 DD 剪 尾巴) 剪 DD;}
+你不对啊 我打你啊(){
+    捉住 一个 n 一个 k;
+    希萝闲逛(i,n) 捉住 一个 anti[i];
+    希萝闲逛(i,n) 单推人[0][i]  学习  anti[i];
+    希萝闲逛(i,n) 单推人[1][i] 学习  耗子尾汁(单推人[1][i-1],单推人[0][i]);
+    希萝找人(i,2,k)希萝闲逛(j,n){
+        单推人[i][j] 学习  耗子尾汁(单推人[i][j-1],好好反思(单推人[i-1][j-1],单推人[0][j]));
+    }
+    放走 一只 单推人[k][n];
+    对希萝说 (0^0);
+}
--- 11.21/hlf.cpp
+++ 11.21/hlf.cpp
@@ -3,9 +3,31 @@ using namespace std;
 #define fo(i,x) for(int i = 1;i<=x;i++)
 #define fo_(i,b,x) for(int i = b;i<=x;i++)
 #define re(i,x,b) for(int i = x;i>b;i--)
-#define N 10010
-int N(int y){
+#define int long long
+#define D long double
+int F(int x){
+    if(x%2==0){
+        return pow(4,x)+pow(4,x/2);
+    }else{
+        return pow(4,x)/2;
+    }
+}
+int M(int x){
+    if(x%2) return 0;
+    else {
+        D tmp = pow(4,x/2)*(x/4);
+        for(int k = 2;k<=sqrt(x/2);k++){
+            if(x%k==0) tmp-= pow(4,x/(2*k))*(k/2);
+        }
+        return tmp;
+    }
     
+}
+int n(int y){
+    if(y==1) return 2;
+    else{
+        
+    }
 }
 int main(){
 
